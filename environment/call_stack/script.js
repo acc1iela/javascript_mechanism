31ae13cb08d49049ee16eac68519e62b420f58f7
acc1iela <acc1iela@gmail.com>
2021-01-13 01:21:42 +0900

    テンプレートファイル取り込み

--- environment/call_stack/script.js
+++
@@ -1,9 +0,0 @@
-function a() {}
-
-function b() {}
-
-function c() {
-  b();
-}
-
-c();
--- environment/context/script.js
+++
@@ -1,7 +0,0 @@
-let a = 0; // これが外部変数に該当する
-function b() {
-  console.log(this, arguments, a);
-}
-
-console.log(a);
-b();
